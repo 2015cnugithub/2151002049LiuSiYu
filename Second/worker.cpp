f504a1769adc6a65c4bfb19e495f23a35b9747ca
cnugit(lsycnu) <443621808@qq.com>
2015-10-27 21:42:44 +0800

    My work

--- Second/worker.cpp
+++ Second/worker.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 using namespace std;
+class Worker;
+ostream & operator<<(ostream &out,Worker& worker);
 class Worker{
 public:
 	virtual void Compute_pay(double hours)=0;
@@ -14,7 +16,7 @@ public:
 Worker::Worker(char *a,int b,char *c,int d){
       name=a;
 	  age=b;
-	  c=sex;
+	  sex=c;
 	  pay_per_hour=d;
 }
 
@@ -42,7 +44,7 @@ void SalariedWorker::Compute_pay(double hours)
 	else
 		cout<<(pay_per_hour*hours+0.5*pay_per_hour*(35-hours))<<endl;
 }
-ostream & operator<<(ostream &out,Worker& worker){
+ostream & operator<<(ostream &out,Worker  &worker){
 	    cout<<"aaaa"<<endl;
         out<<"姓名："<<worker.name<<" 年龄："<<worker.age<<" 性别"<<worker.sex<<" 小时工资额:"<<worker.pay_per_hour<<endl;
         return out;
@@ -50,38 +52,40 @@ ostream & operator<<(ostream &out,Worker& worker){
 int main()
 {
 	Worker* workers[5];
-	int m,n;
+	int m,n,i;
 	char name[5][10];
 	char sex[5][10];
 	int a,b;
+
 	cout<<"输入5个不同工人的基本信息"<<endl;
-	for(int i=0;i<5;i++)
+	for( i=0;i<5;i++)
 	{
 		cin>>name[i]>>a>>sex[i]>>b;
 		if(b==10||b==20||b==40)
-		{	HourlyWorker a(name[i],a,sex[i],b);workers[i]=&a;}
+		{	workers[i]=new HourlyWorker (name[i],a,sex[i],b);}
 		else
-		{  SalariedWorker a(name[i],a,sex[i],b);workers[i]=&a;}
+		{  workers[i]=new SalariedWorker (name[i],a,sex[i],b);}
 
 	}
 	m=0;
 	while(m!=2)
 	{
 	  cout<<"1.输出工人信息";
-	  cout<<"2.退出";
+	  cout<<"2.退出"<<endl;
 	  cin>>m;
 	  if(m==1)
 	  {
            cout<<"输入员工编号："<<endl;
-			   cin>>n;
-           cout<<workers[i]<<endl;
+			   cin>>i;
+           cout<<*workers[i-1]<<endl;
 		   cout<<"输入小时数："<<endl;
 			   cin>>n;
            cout<<"工资是："<<endl;
-		   workers[i]->Compute_pay(n);
+		   workers[i-1]->Compute_pay(n);
 			  cout<<endl;
 
 	  }
 	}
+
 		return 0;
 }
